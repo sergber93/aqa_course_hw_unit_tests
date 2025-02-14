c0c3ef168d7a36425a4404aba0aefef88321b865
sergber93 <sergber93@gmail.com>
2025-02-14 22:54:11 +0400

    Tasks 1 and 2

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -1,6 +1,21 @@
 /*
 
 Напишите программу, которая принимает целое положительное число n (одно любое число от 1 до 9), и выводит сумму равную 
-n + nn + nnn, где n не перемножаются, а конкатенируются
+n + nn + nnn, где n не перемножаются, а     
 
 */
+const number= prompt("Введите число от 1 до 9",0)
+if(number>0 && number<=9) {
+
+    const numberOne= Number(number)
+    const numberTwo= String(number)+ String(number)
+    const numberThree = String(number)+ String(number) + String(number)
+
+    console.log (numberOne + Number(numberTwo)+Number(numberThree))
+
+}
+else if(typeof number !== 'number'){
+    console.log('Введите число')
+}else{
+console.log('Некорректное число')
+}
