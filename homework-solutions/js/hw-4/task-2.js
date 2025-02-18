cc627e8f3bb13515f50bd65636966a4a2a4a7487
sergber93 <sergber93@gmail.com>
2025-02-18 21:03:52 +0400

    tasks 1-2

--- homework-solutions/js/hw-4/task-2.js
+++ homework-solutions/js/hw-4/task-2.js
@@ -14,3 +14,14 @@
   5. Проверить задание со следующими значениями в переменной age: 10, 17, 18, 19, 59, 60, 61
 
   */
+let minAge, maxAge, age
+minAge = 18
+maxAge = 60
+age = 10
+if (typeof age !== "number") {
+  console.log ("Incorrect data type")
+} else {
+  if( age < minAge ){
+    console.log(`"You don't have access cause your age is ${age} It's less then"`)
+}
+}
