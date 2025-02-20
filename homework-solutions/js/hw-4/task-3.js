038d27b89cbf45adcb5c8c0c98fa7a5d760138a5
sergber93 <sergber93@gmail.com>
2025-02-20 20:54:46 +0400

    Resolved tasks 1-3 HW-4

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,22 @@
   преобразовываясь в number
 
 */
+let minAge, maxAge, age
+minAge = 18
+maxAge = 60
+age = "2"
+age = parseInt(age)
+if (isNaN (age)) {
+  console.log ("Incorrect data type")
+} if ( age < minAge )
+  {
+    console.log(`You don't have access cause your age is ${age} It's less then`)
+}
+else if (age >=minAge && age <= maxAge) {
+console.log ("Welcome")
+}
+else if (age > maxAge) {
+  console.log("Keep calm and look culture channel")
+}
+else {
+  console.log ("Technical work")
